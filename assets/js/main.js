681ec29444e5cf7130bc008fa05fc4db9d44d483
jvjoaoo <jvjoaoo.vitor@gmail.com>
2025-04-01 20:46:16 -0300

    update de funcionalidades do sistema

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+  $("#cnpj").mask("00.000.000/0000-00")
+  $("#telefone").mask("(00) 00000-0000")
+})
 
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    document.getElementById("preloader").style.display = "none"
+    document.getElementById("content").classList.remove("hidden")
+  }, 2000) // Simulando um carregamento de 2 segundos
+})
