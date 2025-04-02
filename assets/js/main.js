536f50c1fadf8205e1476d7741d7c91bec4dae76
jvjoaoo <jvjoaoo.vitor@gmail.com>
2025-04-01 21:01:40 -0300

    comentarios em código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-/* Mascará de formatação para formulário */
+// Mascará de formatação para formulário
 $(document).ready(function () {
   $("#cnpj").mask("00.000.000/0000-00")
   $("#telefone").mask("(00) 00000-0000")
 })
 
-/* Animação de pré-carregamento do site */
+// Animação de pré-carregamento do site
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(() => {
     document.getElementById("preloader").style.display = "none"
     document.getElementById("content").classList.remove("hidden")
-  }, 2000) // Simulando um carregamento de 2 segundos
+  }, 2000)
 })
